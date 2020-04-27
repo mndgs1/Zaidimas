fa892e5f31f7a6416bb14038ce4af022ad596a2b
mndgs1 <mindaugasbankauskas@gmail.com>
2020-04-27 20:15:15 +0200

    Zaidimo Interface

---
+++ ZaidimasGitHub/ZaidimasGitHub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaidimasGitHub
+{
+    interface Zaidimas
+    {
+        int ataka { get; set; }
+        int gyvybes { get; set; }
+        string charName { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
